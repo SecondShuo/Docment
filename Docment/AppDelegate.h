2e70b8a393f307d292dfc94da918b989cfef29dd
echo012 <echo012@bogon>
2015-12-30 10:14:07 +0800

    Initial Commit

---
+++ Docment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Docment
+//
+//  Created by echo012 on 15/12/30.
+//  Copyright © 2015年 张洋硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Docment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Docment
+//
+//  Created by echo012 on 15/12/30.
+//  Copyright © 2015年 张洋硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
